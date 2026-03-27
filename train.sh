a01379de4119c1da8f83b12d43683944eacfc99f
Hamza Khalil <103629235+hamzakhalil798@users.noreply.github.com>
2026-03-27 15:38:06 +0500

    Custom train 250 epochs

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ case "${task}" in
     'DIS5K') epochs=500 && val_last=50 && step=5 ;;
     'COD') epochs=150 && val_last=50 && step=5 ;;
     'HRSOD') epochs=150 && val_last=50 && step=5 ;;
-    'General') epochs=200 && val_last=50 && step=5 ;;
+    'Custom') epochs=$((244+250)) && val_last=250 && step=5 ;;
     'General-2K') epochs=250 && val_last=30 && step=2 ;;
     'Matting') epochs=150 && val_last=50 && step=5 ;;
 esac
@@ -19,21 +19,21 @@ nproc_per_node=$(echo ${devices%%,} | grep -o "," | wc -l)
 to_be_distributed=`echo ${nproc_per_node} | awk '{if($e > 0) print "True"; else print "False";}'`
 
 echo Training started at $(date)
-resume_weights_path='path_to_a_pth'
+resume_weights_path='/workspace/weights/cv/BiRefNet-general-epoch_244.pth'
+ckpt_dir="/workspace/drive/MyDrive/train_birefnet/ckpts/${method}"
 if [ ${to_be_distributed} == "True" ]
 then
-    # Adapt the nproc_per_node by the number of GPUs. Give 8989 as the default value of master_port.
     echo "Multi-GPU mode received..."
     CUDA_VISIBLE_DEVICES=${devices} \
     torchrun --standalone --nproc_per_node $((nproc_per_node+1)) \
-    train.py --ckpt_dir ckpts/${method} --epochs ${epochs} \
+    train.py --ckpt_dir ${ckpt_dir} --epochs ${epochs} \
         --dist ${to_be_distributed} \
         --resume ${resume_weights_path} \
         --use_accelerate
 else
     echo "Single-GPU mode received..."
     CUDA_VISIBLE_DEVICES=${devices} \
-    python train.py --ckpt_dir ckpts/${method} --epochs ${epochs} \
+    python train.py --ckpt_dir ${ckpt_dir} --epochs ${epochs} \
         --dist ${to_be_distributed} \
         --resume ${resume_weights_path} \
         --use_accelerate
