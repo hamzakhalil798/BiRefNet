545900529bf72bad29233169f395dee17bc5b83b
Hamza Khalil <103629235+hamzakhalil798@users.noreply.github.com>
2026-04-06 14:17:26 +0500

    Inference from checkpoint on its own..

--- test.sh
+++ test.sh
@@ -1,25 +1,36 @@
-devices=${1:-0}
-pred_root=${2:-e_preds}
-resolutions=${3:-"config.size"}
+#!/bin/bash
+# Run inference & evaluation script
 
-# Inference
-# resolutions="1024x1024 None"
+method="$1"
+devices=${2:-0}
+pred_root=${3:-e_preds}
+resolutions=${4:-"config.size"}
+
+task=$(python3 config.py --print_task)
+ckpt_folder="/workspace/drive/MyDrive/train_birefnet/ckpts/${method}"
+
+# Create pred_root early so eval doesn't fail
+mkdir -p ${pred_root}
+
+echo Inference started at $(date)
+
 for resolution in ${resolutions}; do
-    CUDA_VISIBLE_DEVICES=${devices} python inference.py --pred_root ${pred_root} --resolution ${resolution}
+    CUDA_VISIBLE_DEVICES=${devices} python inference.py \
+        --pred_root ${pred_root} \
+        --resolution ${resolution} \
+        --ckpt_folder ${ckpt_folder}
 done
 
 echo Inference finished at $(date)
 
 # Evaluation
-log_dir=e_logs && mkdir ${log_dir}
+log_dir=e_logs && mkdir -p ${log_dir}
 
-task=$(python3 config.py --print_task)
 testsets=$(python3 config.py --print_testsets)
-
 testsets=(`echo ${testsets} | tr ',' ' '`) && testsets=${testsets[@]}
 
 for testset in ${testsets}; do
     python eval_existingOnes.py --pred_root ${pred_root} --data_lst ${testset} --metrics 'all' > ${log_dir}/eval_${testset}.out
 done
 
-echo Evaluation started at $(date)
+echo Evaluation finished at $(date)
