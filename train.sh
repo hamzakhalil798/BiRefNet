23d2cdf2c6bc32a6561fff3c3414630e3f2fe062
Hamza Khalil <103629235+hamzakhalil798@users.noreply.github.com>
2026-04-06 14:19:22 +0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ case "${task}" in
     'DIS5K') epochs=500 && val_last=50 && step=5 ;;
     'COD') epochs=150 && val_last=50 && step=5 ;;
     'HRSOD') epochs=150 && val_last=50 && step=5 ;;
-    'Custom') epochs=$((244+250)) && val_last=250 && step=5 ;;
+    'Custom') epochs=$((5+100)) && val_last=100 && step=1 ;;
     'General-2K') epochs=250 && val_last=30 && step=2 ;;
     'Matting') epochs=150 && val_last=50 && step=5 ;;
 esac
@@ -19,10 +19,11 @@ nproc_per_node=$(echo ${devices%%,} | grep -o "," | wc -l)
 to_be_distributed=`echo ${nproc_per_node} | awk '{if($e > 0) print "True"; else print "False";}'`
 
 echo Training started at $(date)
-resume_weights_path='/workspace/weights/cv/BiRefNet-general-epoch_244.pth'
+resume_weights_path='/workspace/drive/MyDrive/train_birefnet/ckpts/background_removal_interior/epoch_5.pth'
 ckpt_dir="/workspace/drive/MyDrive/train_birefnet/ckpts/${method}"
 if [ ${to_be_distributed} == "True" ]
 then
+    # Adapt the nproc_per_node by the number of GPUs. Give 8989 as the default value of master_port.
     echo "Multi-GPU mode received..."
     CUDA_VISIBLE_DEVICES=${devices} \
     torchrun --standalone --nproc_per_node $((nproc_per_node+1)) \
